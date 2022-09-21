cd70bd601ee20f7b3919ed7d04aacb07a09ad4a0
Maksym Solomko <max.exc7@gmail.com>
2022-09-21 23:26:06 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".js-gallery");
+
+const galleryBox = ({ preview, original, description }) => `
+<a href="${original}">
+  <img
+    src="${preview}"
+    alt="${description}"
+  />
+</a>`;
+
+const makeImageCard = () => {
+  const imageList = galleryItems.map((img) => galleryBox(img)).join("");
+
+  galleryList.innerHTML = "";
+  galleryList.insertAdjacentHTML("beforeend", imageList);
+};
+
+console.log(makeImageCard());
