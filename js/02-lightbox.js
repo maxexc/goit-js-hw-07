56260f2935e69f17c0cbbd1118f80309f005d471
Maksym Solomko <max.exc7@gmail.com>
2022-09-22 01:22:21 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".js-gallery");
+
+galleryRef.innerHTML = "";
+galleryRef.insertAdjacentHTML("beforeend", makeImageList(galleryItems));
+galleryRef.addEventListener("click", onImageClick);
+
+function makeImageList(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `      
+        <a href="${original}" class="gallery__item">
+        <img
+          class="gallery__image"
+          src=${preview}                       
+          alt=${description}
+        />
+        </a>`
+    )
+    .join("");
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+  let gallery = new SimpleLightbox(".js-gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
