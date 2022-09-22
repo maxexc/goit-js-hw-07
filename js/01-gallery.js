1c512a034d3e7627b7d56d6c1f5372801579a545
Maksym Solomko <max.exc7@gmail.com>
2022-09-22 22:17:30 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -37,9 +37,18 @@ function onImageClick(event) {
     `);
   instance.show();
 
-  document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
+  document.addEventListener("keydown", closeImgCard);
+
+  function closeImgCard(event) {
+    if (event.code === "Escape") {
       instance.close();
+      document.removeEventListener("keydown", closeImgCard);
     }
-  });
+  }
+
+  // document.addEventListener("keydown", (event) => {
+  //   if (event.key === "Escape") {
+  //     instance.close();
+  //   }
+  // });
 }
